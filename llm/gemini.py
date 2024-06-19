fa5d1187c9e57b2108421c1891363641c693ead1
tjouliab <tim.jouliab@icloud.com>
2024-06-19 11:03:35 +0200

    sync section 4

--- llm/gemini.py
+++ llm/gemini.py
@@ -1,12 +1,28 @@
 import os
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings, HarmBlockThreshold, HarmCategory
 from dotenv import load_dotenv
+from langchain import prompts
 
 load_dotenv()
 API_KEY = os.getenv("GOOGLE_API_KEY")
 MODEL_NAME = os.getenv("GOOGLE_MODEL_NAME")
+EMBEDDINGS_MODEL = os.getenv("GOOGLE_EMBEDDINGS_MODEL")
 
 model = ChatGoogleGenerativeAI(
     model=MODEL_NAME,
     google_api_key=API_KEY,
+    safety_settings={
+        HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
+        HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
+        HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
+        HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE
+    },
 )
+
+embeddings = GoogleGenerativeAIEmbeddings(model=EMBEDDINGS_MODEL)
+
+def function(val: int):
+    val2 = 4
+    val1 = val2
+    print(val)
+    print(val2)
--- main.py
+++ main.py
@@ -1,4 +1,71 @@
 import os
+from langchain import document_loaders, text_splitter, chains, schema
+from langchain.vectorstores.chroma import Chroma
+from langchain.embeddings.base import Embeddings
+from llm import gemini
+from langchain.globals import set_debug, set_verbose
+
+set_debug(True)
+set_verbose(True)
+
+
+class RedundantFilterRetriever(schema.BaseRetriever):
+    embeddings: Embeddings
+    chroma: Chroma
+    k: int
+
+    def get_relevant_documents(self, query):
+        emb = self.embeddings.embed_query(query)
+        return self.chroma.max_marginal_relevance_search_by_vector(
+            embedding=emb,
+            lambda_mult=0.8,
+            k=self.k,
+        )
+
+    def aget_relevant_documents(self, query):
+        return []
+
+
+def create_db(embeddings):
+    splitter = text_splitter.CharacterTextSplitter(
+        separator="\n",
+        chunk_size=200,
+        chunk_overlap=0,
+    )
+
+    loader = document_loaders.TextLoader("./section4/facts.txt")
+    docs = loader.load_and_split(text_splitter=splitter)
+    db = Chroma.from_documents(
+        docs, embedding=embeddings, persist_directory="./section4/emb"
+    )
+
+
+def get_retriever(embeddings):
+    db = Chroma(
+        persist_directory="./section4/emb",
+        embedding_function=embeddings,
+    )
+
+    # retriever = db.as_retriever(search_kwargs={"k": 4})
+    retriever = RedundantFilterRetriever(embeddings=embeddings, chroma=db, k=4)
+
+    return retriever
+
 
 if __name__ == "__main__":
-    pass
+    # Get the gemini model and embeddings
+    model = gemini.model
+    embeddings = gemini.embeddings
+
+    # create_db(embeddings=embeddings)
+    retriever = get_retriever(embeddings=embeddings)
+
+    chain = chains.RetrievalQA.from_chain_type(
+        llm=model,
+        retriever=retriever,
+        chain_type="map_reduce",
+    )
+
+    result = chain.run("What is an interesting fun fact about a great scientist ?")
+
+    print(result)
